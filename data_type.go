0e8d5720168d7b449b53f2676541db1117ff6c08
ZiaKfa <zia14148@gmail.com>
2023-12-29 23:51:31 +0700

    add new lesson

--- data_type.go
+++ data_type.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 func main() {
+	//String
 	var helo string = "Hello World"
-	fmt.Println(helo + " adalah string")
-	fmt.Println(helo[0], "adalah byte binary char index ke 0 dari string ", helo)
-	fmt.Println(len(helo), "adalah panjang dari string ", helo)
+	fmt.Println(helo + " is a string")
+	fmt.Println(helo[0], "is a binary byte of first index char from ", helo, " string")
+	fmt.Println(len(helo), "is the length of string ", helo)
+
+	//Number
 	var num int32 = 10
-	fmt.Println(num, "adalah integer")
+	fmt.Println(num, "is an integer")
 	var num2 int64 = 1000000000000000000
-	fmt.Println(num2, "adalah integer 64 bit")
+	fmt.Println(num2, "is a 64 bit integer ")
+
+	//Float
+	var phiShort float32 = 3.14
+	fmt.Println(phiShort, "is a 32 bit float ")
+	var phiLong float64 = 3.14159265358979323846264338327950288419716939937510582097494459
+	fmt.Println(phiLong, "is a 64 bit float ")
+
+	//Boolean
 	var betul bool = true
-	fmt.Println(betul, "adalah boolean")
+	fmt.Println(betul, "is a boolean")
 	var salah bool = false
-	fmt.Println(salah, "adalah boolean")
+	fmt.Println(salah, "is a boolean")
 }
--- variable.go
+++ variable.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	//complete variable declaration
+	var hello string = "Hello World"
+	fmt.Println(hello + " is a string")
+
+	//variable declaration without type inference, automatically inferred
 	var name = "John Doe"
 	fmt.Println("Hello", name)
 	name = "Jane Doe"
 	fmt.Println("Hello", name)
+
+	//variable declaration without var keyword, automatically inferred
+	age := 20
+	fmt.Println("Age", age)
+	age = 30
+	fmt.Println("Age", age)
+
+	//multiple variable declaration
+	var (
+		firstName = "John"
+		lastName  = "Doe"
+	)
+	fmt.Println("Hello", firstName, lastName)
 }
